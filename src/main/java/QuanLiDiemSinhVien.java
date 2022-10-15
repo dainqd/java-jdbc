7b88bb84bbfd4e0210411575de174254d897a054
DiepHoang19 <93962691+DiepHoang19@users.noreply.github.com>
2022-10-15 10:55:20 +0700

    upadte

--- src/main/java/QuanLiDiemSinhVien.java
+++ src/main/java/QuanLiDiemSinhVien.java
@@ -1,31 +1,74 @@
 import entity.DMKhoa;
+import entity.DMMonHoc;
+import entity.DMSinhVien;
+import entity.KetQua;
 
 public class QuanLiDiemSinhVien {
     public static void main(String[] args) {
         DMKhoa dmKhoa = new DMKhoa();
-        System.out.println("");
-        dmKhoa.createManyKhoa();
-        dmKhoa.readKhoa();
+//        System.out.println("");
+//        dmKhoa.createManyKhoa();
+//        dmKhoa.readKhoa();
         System.out.println();
         dmKhoa.createKhoa();
         dmKhoa.readKhoa();
-        System.out.println();
-        dmKhoa.updateKhoa();
-        dmKhoa.readKhoa();
-        System.out.println();
-        dmKhoa.deleteKhoa();
-        dmKhoa.readKhoa();
+//
+//        System.out.println();
+//        dmKhoa.updateKhoa();
+//        dmKhoa.readKhoa();
+//
+//        System.out.println();
+//        dmKhoa.deleteKhoa();
+//        dmKhoa.readKhoa();
+
+//
+//        DMMonHoc dmMonHoc = new DMMonHoc();
+//        System.out.println("");
+//        dmMonHoc.createMonHoc();
+//        dmMonHoc.readMonHoc();
+//        System.out.println();
+//        dmMonHoc.createManyMonHoc();
+//        dmMonHoc.readMonHoc();
+//        System.out.println();
+//        dmMonHoc.updateMonHoc();
+//        dmMonHoc.readMonHoc();
+//        System.out.println();
+//        dmMonHoc.deleteMonHoc();
+//        dmMonHoc.readMonHoc();
+
+//
+//        DMSinhVien dmSinhVien = new DMSinhVien();
+//        System.out.println("");
+//        dmSinhVien.createSinhVien();
+//        System.out.println("");
+//        dmSinhVien.readSinhVien();
+//        System.out.println();
+//        dmSinhVien.createManySinhVien();
+//        System.out.println("");
+//        dmSinhVien.readSinhVien();
+//        System.out.println();
+//        dmSinhVien.updateSinhVien();
+//        System.out.println("");
+//        dmSinhVien.readSinhVien();
+//        System.out.println();
+//        dmSinhVien.deleteSinhVien();
+//        System.out.println("");
+//        dmSinhVien.readSinhVien();
 
 
-
-
-
-
-
-
-
-
-
+//        KetQua ketQua = new KetQua();
+//        System.out.println("");
+//        ketQua.createKetQua();
+//        ketQua.readKetQua();
+//        System.out.println();
+//        ketQua.createManyKetQua();
+//        ketQua.readKetQua();
+//        System.out.println();
+//        ketQua.updateKetQua();
+//        ketQua.readKetQua();
+//        System.out.println();
+//        ketQua.deleteKetQua();
+//        ketQua.readKetQua();
 
 
     }
--- src/main/java/entity/DMMonHoc.java
+++ src/main/java/entity/DMMonHoc.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class DMMonHoc implements DMMonHocDAO {
     @Override
     public void createMonHoc() {
-        String sqlInsert = "INSERT INTO dmmonhoc VALUES(?, ?)";
+        String sqlInsert = "INSERT INTO dmmonhoc VALUES(?, ? , ?)";
         try (
                 Connection connection = DriverManager.getConnection(
                         "jdbc:mysql://localhost:3306/student",
@@ -23,14 +23,17 @@ public class DMMonHoc implements DMMonHocDAO {
             String createMonhocID = scanner.next();
             System.out.println("Enter monhoc colum to create: ");
             String createMonhoc = scanner.next();
+            System.out.println("Enter KhoaMonhoc colum to create: ");
+            String createKhoaMonhoc = scanner.next();
 
             if (createMonhocID != null || createMonhocID != "") {
                 preparedStatement.setString(1, createMonhocID);
                 preparedStatement.setString(2, createMonhoc);
+                preparedStatement.setString(3, createKhoaMonhoc);
                 int rowInsert = preparedStatement.executeUpdate();
                 System.out.println(rowInsert + " Colum create done");
             } else {
-                System.out.printf("Vui long nhap lai MonhocID va mon hoc");
+                System.out.printf("Vui long nhap lai ");
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -39,7 +42,7 @@ public class DMMonHoc implements DMMonHocDAO {
 
     @Override
     public void updateMonHoc() {
-        String sqlUpdate = "UPDATE dmmonhoc SET monhoc = ? WHERE monhocID = ?";
+        String sqlUpdate = "UPDATE dmmonhoc SET TenMonhoc = ? WHERE MonHocID = ?";
         try (
                 Connection connection = DriverManager.getConnection(
                         "jdbc:mysql://localhost:3306/student",
@@ -50,13 +53,16 @@ public class DMMonHoc implements DMMonHocDAO {
         ) {
 
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter khoaID colum to update: ");
+            System.out.println("Enter idMonhoc colum to update: ");
             String idMONHOC = scanner.next();
-            System.out.println("Enter nameKhoa colum to update: ");
+            System.out.println("Enter updateMonhoc colum to update: ");
             String updateMonhoc = scanner.next();
+            System.out.println("Enter nameKhoa colum to update: ");
+            String updateKhoaMonhoc = scanner.next();
 
             preparedStatement.setString(1, updateMonhoc);
             preparedStatement.setString(2, idMONHOC);
+            preparedStatement.setString(3, updateKhoaMonhoc);
 
             int rowDelete = preparedStatement.executeUpdate();
             System.out.println(rowDelete + " Colum update done");
@@ -68,7 +74,7 @@ public class DMMonHoc implements DMMonHocDAO {
 
     @Override
     public void deleteMonHoc() {
-        String sqlDelete = "delete from dmmonhoc where monhocID = ?";
+        String sqlDelete = "delete from dmmonhoc where MonHocID = ?";
         try (
                 Connection connection = DriverManager.getConnection(
                         "jdbc:mysql://localhost:3306/student",
@@ -106,9 +112,10 @@ public class DMMonHoc implements DMMonHocDAO {
 
             ResultSet resultset = preparedStatement.executeQuery();
             while (resultset.next()) {
-                String monhocID = resultset.getString("khoaID");
-                String monhoc = resultset.getString("nameKhoa");
-                System.out.println("DMKhoa:" + monhocID + ", " + monhoc);
+                String monhocID = resultset.getString("MonHocID");
+                String monhoc = resultset.getString("TenMonhoc");
+                String KhoaMonhoc = resultset.getString("KhoaMonhoc");
+                System.out.println("dmmonhoc:" + monhocID + ", " + monhoc + "," + KhoaMonhoc);
             }
 
         } catch (SQLException e) {
@@ -118,7 +125,7 @@ public class DMMonHoc implements DMMonHocDAO {
 
     @Override
     public void createManyMonHoc() {
-        String sqlInsert = "INSERT INTO dmmonhoc VALUES(?, ?)";
+        String sqlInsert = "INSERT INTO dmmonhoc VALUES(?, ? , ?)";
         try (
                 Connection connection = DriverManager.getConnection(
                         "jdbc:mysql://localhost:3306/student",
@@ -127,48 +134,57 @@ public class DMMonHoc implements DMMonHocDAO {
 
                 PreparedStatement preparedStatement = connection.prepareStatement(sqlInsert);
         ) {
-
             Scanner scanner = new Scanner(System.in);
             System.out.println("Enter monhocID colum to create: ");
             String createmonhocID = scanner.next();
             System.out.println("Enter monhoc colum to create: ");
             String createmonhoc = scanner.next();
+            System.out.println("Enter monhoc colum to create: ");
+            String createkhoamonhoc = scanner.next();
+
 
             System.out.println("Enter monhocID 2 colum to create: ");
             String createmonhocID1 = scanner.next();
             System.out.println("Enter monhoc 2 colum to create: ");
             String createmonhoc1 = scanner.next();
+            System.out.println("Enter createkhoamonhoc 2 colum to create: ");
+            String createkhoamonhoc1 = scanner.next();
 
             System.out.println("Enter monhocID 3 colum to create: ");
             String createmonhocID2 = scanner.next();
             System.out.println("Enter monhoc 3 colum to create: ");
             String createmonhoc2 = scanner.next();
+            System.out.println("Enter createkhoamonhoc 3  colum to create: ");
+            String createkhoamonhoc2 = scanner.next();
 
             if (createmonhocID != null || createmonhocID != "") {
                 preparedStatement.setString(1, createmonhocID);
                 preparedStatement.setString(2, createmonhoc);
+                preparedStatement.setString(3, createkhoamonhoc);
                 int rowInsert = preparedStatement.executeUpdate();
                 System.out.println(rowInsert + " Colum create done");
             } else {
-                System.out.printf("Vui long nhap lai monhocID va monhoc");
+                System.out.printf("Vui long nhap lai ");
             }
 
             if (createmonhocID1 != null || createmonhocID1 != "") {
                 preparedStatement.setString(1, createmonhocID1);
                 preparedStatement.setString(2, createmonhoc1);
+                preparedStatement.setString(3, createkhoamonhoc1);
                 int rowInsert = preparedStatement.executeUpdate();
                 System.out.println(rowInsert + " Colum create done");
             } else {
-                System.out.printf("Vui long nhap lai monhocID va monhoc");
+                System.out.printf("Vui long nhap lai ");
             }
 
             if (createmonhocID2 != null || createmonhocID2 != "") {
                 preparedStatement.setString(1, createmonhocID2);
                 preparedStatement.setString(2, createmonhoc2);
+                preparedStatement.setString(3, createkhoamonhoc2);
                 int rowInsert = preparedStatement.executeUpdate();
                 System.out.println(rowInsert + " Colum create done");
             } else {
-                System.out.printf("Vui long nhap lai monhocID va monhoc");
+                System.out.printf("Vui long nhap lai ");
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
